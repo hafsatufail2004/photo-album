31a2e43ddf1c5a171ba2f2ceadf8de8db94f8d39
hafsatufail2004 <tufailhafsa12@gmail.com>
2023-08-22 16:47:39 +0500

    working on gallery page

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { CldUploadButton } from "next-cloudinary";
+import { UploadResult } from "../page";
+import { Button } from "@/components/ui/button";
+
+export default function GalleryPage(){
+return <section>
+<div className="flex justify-between ">
+<h1 className="text-4xl font-bold">Gallery</h1>
+ <Button asChild>
+    <div className="gap-x-2">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+</svg>
+
+
+<CldUploadButton
+ onUpload = {(result:UploadResult)=>{
+//setImageID(result.info.public_id);
+ }}
+ uploadPreset="dxmscjbj" />
+ </div>
+ </Button>
+
+</div>
+
+</section>
+}
